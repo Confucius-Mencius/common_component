f1fc1ceac1269fe9e0c0cc599bbeb3c1e59cc939
hgc <guang11cheng@qq.com>
2019-05-29 10:01:12 +0800

    增加s3测试用例

---
+++ base/test/aws/s3_test.h
@@ -0,0 +1,18 @@
+#ifndef BASE_TEST_CPP11_ALLOCATOR_TEST_H_
+#define BASE_TEST_CPP11_ALLOCATOR_TEST_H_
+
+#include "test_util.h"
+
+class AllocatorTest : public GTest
+{
+public:
+    AllocatorTest();
+    virtual ~AllocatorTest();
+
+    void Test001();
+
+private:
+
+};
+
+#endif // BASE_TEST_CPP11_ALLOCATOR_TEST_H_
